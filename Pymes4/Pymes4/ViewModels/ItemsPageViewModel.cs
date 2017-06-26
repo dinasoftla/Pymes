9087248e8127cddbcb903cf1ce27d288531a5e7d
Ernesto Burbano <ernesto_bur@hotmail.com>
2017-06-26 19:50:35 +0200

    CheckPoint ok

---
+++ Pymes4/Pymes4/ViewModels/ItemsPageViewModel.cs
@@ -0,0 +1,166 @@
+﻿using Pymes4.Classes;
+using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Windows.Input;
+using Pymes4.Helpers;
+
+namespace Pymes4.ViewModels
+{
+    class ItemsPageViewModel : INotifyPropertyChanged
+    {
+        #region Attributes
+
+        private RootObjectUsuarios usuarios;
+
+        private bool isRunning;
+
+        private bool isEnabled;
+
+        private string message;
+
+        #endregion
+
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Properties
+
+        public bool IsRunning
+        {
+            set
+            {
+                if (isRunning != value)
+                {
+                    isRunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsRunning"));
+                }
+            }
+            get
+            {
+                return isRunning;
+            }
+        }
+
+        public bool IsEnabled
+        {
+            set
+            {
+                if (isEnabled != value)
+                {
+                    isEnabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsEnabled"));
+                }
+            }
+            get
+            {
+                return isEnabled;
+            }
+        }
+        public string Message
+        {
+            set
+            {
+                if (message != value)
+                {
+                    message = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Message"));
+                }
+            }
+            get
+            {
+                return message;
+            }
+        }
+        #endregion
+
+        #region Constructors
+
+        public ItemsPageViewModel()
+        {
+            IsEnabled = false;
+            //GetRates();
+            LoadApiResult(Settings.Phone);
+            Message = "Select the values";
+
+        }
+        #endregion
+
+        #region Commands
+        #endregion
+
+        #region Methods
+        public async void LoadApiResult(string phone)
+        {
+            //if (!String.IsNullOrEmpty(Settings.Phone))
+            //{
+            try
+            {
+                IsRunning = true;
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://192.168.0.10");
+                string url = string.Format("/apirest/index.php/consultacliente/{0}", phone);
+                var response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    await App.Current.MainPage.DisplayAlert("Error", response.StatusCode.ToString(), "Aceptar");
+                    IsRunning = false;
+                    IsEnabled = false;
+                    return;
+                }
+                else
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    //Crear clase interface para notificaciones:
+                    usuarios = JsonConvert.DeserializeObject<RootObjectUsuarios>(result);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Error De Conexión", ex.Message, "Aceptar");
+                IsRunning = false;
+                IsEnabled = false;
+                return;
+            }
+            //
+
+            Successful();
+            IsRunning = false;
+            IsEnabled = true;
+            //}
+
+        }
+
+
+        private void Successful()
+        {
+            if (usuarios.Usuarios[0].usuarioactivo == "1")
+            {
+                App.Current.MainPage = new Pages.MenuPage();
+            }
+            else if (usuarios.Usuarios[0].usuarioactivo == "0")
+            {
+                App.Current.MainPage = new Pages.InactiveUsr();
+            }
+            Settings.Name = usuarios.Usuarios[0].nombre;
+            Settings.Phone = usuarios.Usuarios[0].telefono;
+            Settings.Email = usuarios.Usuarios[0].email;
+            Settings.ActiveUser = usuarios.Usuarios[0].usuarioactivo;
+            Settings.Appointment = usuarios.Usuarios[0].cita;
+            Settings.AppointmentStatus = usuarios.Usuarios[0].estadocita;
+            Settings.Offert = usuarios.Usuarios[0].oferta;
+        }
+
+
+        #endregion
+    }
+}
